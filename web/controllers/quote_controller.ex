9bafe38cd7c18f121c307c34bcdb3f3a8fb46bac
hiphoox <norberto.ortigoza@raku.mx>
2015-02-01 17:02:50 -0600

    Updating Views

--- web/controllers/quote_controller.ex
+++ web/controllers/quote_controller.ex
@@ -1,10 +1,15 @@
 defmodule PhoenixCrud.QuoteController do
   use Phoenix.Controller
 
+  alias PhoenixCrud.Router
+  import PhoenixCrud.Router.Helpers
+
   plug :action
 
   def homepage(conn, _params) do
-    render conn, "homepage.html"
+    conn
+    |> assign(:quote, PhoenixCrud.Quote.Queries.random)
+    |> render("show.html")
   end
   
   def index(conn, _params) do
@@ -13,4 +18,44 @@ defmodule PhoenixCrud.QuoteController do
     |> render("index.html")
   end
   
+  def new(conn, _params) do
+    render conn, "new.html"
+  end
+
+  def create(conn, %{"quote" => %{"saying" => saying, "author" => author}}) do
+    q = %PhoenixCrud.Quote{saying: saying,  author: author}
+    Repo.insert(q)
+
+    redirect conn, to: quote_path(conn, :index)
+  end
+
+  def show(conn, %{"id" => id}) do
+    {id, _} = Integer.parse(id)
+    conn
+    |> assign(:quote, Repo.get(PhoenixCrud.Quote, id))
+    |> render("show.html")
+  end
+
+  def edit(conn, %{"id" => id}) do
+    {id, _} = Integer.parse(id)
+    conn
+    |> assign(:quote, Repo.get(PhoenixCrud.Quote, id))
+    |> render("edit.html")
+  end
+
+  def update(conn, %{"id" => id, "quote" => %{"saying" => saying, "author" => author}}) do
+    {id, _} = Integer.parse(id)
+    q = Repo.get(PhoenixCrud.Quote, id)
+    q = %{q | saying: saying, author: author }
+    Repo.update(q)
+    redirect conn, to: quote_path(conn, :show, q.id)
+  end
+
+  def destroy(conn, %{"id" => id}) do
+    {id, _} = Integer.parse(id)
+    q = Repo.get(PhoenixCrud.Quote, id)
+    Repo.delete(q)
+    redirect conn, to: quote_path(conn, :index)
+  end
+
 end
